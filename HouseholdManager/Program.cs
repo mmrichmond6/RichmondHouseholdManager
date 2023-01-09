2c902702392c06727587932b5b2f1b35f8550b2c
Michele Richmond <mmrichmond6@gmail.com>
2023-01-09 11:05:43 -0600

    Changed Sidebar to not be in _Layout

--- HouseholdManager/Program.cs
+++ HouseholdManager/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
